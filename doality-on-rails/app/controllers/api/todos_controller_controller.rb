8716dea1bb8fb8ff95cf7856b277831eaafb9802
Cassandra <uncertainkitten@gmail.com>
2018-10-02 15:08:09 -0700

    Add API controller methods for Todo

---
+++ doality-on-rails/app/controllers/api/todos_controller_controller.rb
@@ -0,0 +1,36 @@
+class Api::TodosControllerController < ApplicationController
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def index
+    render json: Todo.all
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+  end
+
+  def update
+    @todo = Todo.find_by(id: params[:id])
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy
+    render json: Todo.all
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :body, :done)
+  end
+end
